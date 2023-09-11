414a6af0c699561a6c763521a20848eecd25f056
Rafah <rafatramoni15@gmail.com>
2023-09-11 18:00:22 +0100

    Completed the quiz logic implementation

--- js/app.js
+++ js/app.js
@@ -7,34 +7,41 @@ const startQuizBtn = document.querySelector('.start_btn');
 
 const currentQ = document.querySelector('.current');
 const totaltQ = document.querySelector('.total');
-const progress = document.querySelector('.progress-bar .progress')
-const questionDiv = document.querySelector('.question')
-const options = document.querySelector('.options')
+const progress = document.querySelector('.progress-bar .progress');
+const questionDiv = document.querySelector('.question');
+let optionsDiv = document.querySelector('.options');
+const nextBtn = document.querySelector('.quiz_page .next_btn')
+
 
 
 const questions = [];
 
+const INCREMENT_SCORE_BY = 10;
+let TOTAL_SCORE = 0;
+
+let current_question_number = 0,
+CURRENT_SCORE = 0;
+
 
 // document.addEventListener('load',)
-
-const INCREMENT_SCORE_BY = 10;
-const TOTAL_SCORE =  INCREMENT_SCORE_BY * questions.length;
-let current_question_number = 15,
-score_counter;
-
 function fetchQuestions(){
     fetch("./js/question.json", {mode:"no-cors"}).then((response )=>{
         return response.json();
     }).then((data) => {
        questions.push(...data)
+        TOTAL_SCORE = INCREMENT_SCORE_BY * questions.length;
     })
 }
 fetchQuestions()
 
 
+
+
+
 startQuizBtn.addEventListener('click', initializeQuiz)
 
-function initializeQuiz(){
+async function initializeQuiz(){
+    console.log(TOTAL_SCORE);
     startPage.classList.add('hide-pages');
     quizPage.classList.remove('hide-pages');
         
@@ -46,17 +53,37 @@ function initializeQuiz(){
 
     showQuestion(questionDiv, current_question_number);
 
-    console.log(options);
-    options.innerHTML = questions[current_question_number].map((option)=>{
-        return `
-        <li>${option.choice1}</li>
-        <li>${option.choice2}</li>
-        <li>${option.choice3}</li>
-        <li>${option.choice4}</li>
-        `
+    showOptions(optionsDiv, current_question_number);
 
+    optionsDiv.addEventListener('click', function(e){
+        if (e.currentTarget.getAttribute('disabled') === 'true') { return; }
+
+        const  element = e.target;
+        let optionList = optionsDiv.querySelectorAll('li');
+        
+        if(element.tagName === "LI"){
+           let optionValue = e.target.getAttribute('dataSet');
+          let answer = questions[current_question_number].answer;
+      
+          optionsDiv.setAttribute('disabled', true); 
+
+          checkAnswer(answer, optionValue, element, optionList);
+  
+
+        }
     })
-    
+
+    nextBtn.addEventListener('click', function () {
+        current_question_number++
+        optionsDiv.removeAttribute('disabled');
+        showQuestionCount(currentQ, current_question_number);
+        showProgress(progress, current_question_number);
+        showQuestion(questionDiv, current_question_number);
+        showOptions(optionsDiv, current_question_number);
+        nextBtn.setAttribute('disabled', true);
+        showResultPage(current_question_number);
+    })
+   
 }
 
 function showProgress(element, number){
@@ -65,11 +92,56 @@ function showProgress(element, number){
 }
 
 function showQuestionCount(element, number){
+    if(number > questions.length -1){return}
     element.innerText = questions.indexOf(questions[number]) + 1;
 }
 
 function showQuestion(element, number){
-    element.innerText = questions[number].question
+    if(number < questions.length){
+        element.innerText = questions[number].question
+    }
+    return;
+}
+
+function showOptions(element, number){
+    if(number > questions.length -1){return}
+    let {question, answer, ...options} = questions[number];
+
+   let newOptions = "";
+   let num =1;
+   Object.keys(options).forEach(choice => {
+     newOptions +=`<li  dataSet=${num++}>${options[choice]}</li>`
+   });
+   element.innerHTML = newOptions;
+}
+
+
+function checkAnswer(answer, optionValue, element, optionList){
+    if(answer === Number(optionValue)){
+        element.classList.add('correct');
+        CURRENT_SCORE += INCREMENT_SCORE_BY;
+        console.log(CURRENT_SCORE);
+     }else{
+        element.classList.add('incorrect');
+        optionList.forEach((li) => {
+            if(Number(li.getAttribute('dataSet')) === answer){
+                li.classList.add('correct')
+            }
+         });
+         
+    }
+    nextBtn.removeAttribute('disabled');
+}
+
+function showResultPage(current_question_number) {
+    if(current_question_number >= questions.length -1){
+        current_question_number= 0;
+        quizPage.classList.add('hide-pages');
+        resultPage.classList.remove('hide-pages');
+
+        resultPage.querySelector('.score').innerText= CURRENT_SCORE;
+        resultPage.querySelector('.total_score').innerText = TOTAL_SCORE;
+    }
 }
 
 
